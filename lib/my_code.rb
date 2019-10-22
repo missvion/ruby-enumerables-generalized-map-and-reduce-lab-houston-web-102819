240dab08c9a775b891f0605067367766fe0fd7a0
Vivian Tran <viviantran94@gmail.com>
2019-10-22 03:34:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,6 +12,17 @@ end
 
 
 def reduce(array, starting_point=nil)
-  
+  if starting_point
+    sum = starting_point
+    index = 0
+  else
+    sum = array[0]
+    index = 1
+  end
+  while index < array.length do
+    sum = yield(sum, array[index])
+    index += 1
+  end
+  sum
 end
   
