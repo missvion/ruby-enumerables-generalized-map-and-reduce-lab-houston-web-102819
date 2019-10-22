996795986cf4b9b2dd14c2502b3d9d0b953b0a47
Vivian Tran <viviantran94@gmail.com>
2019-10-22 03:34:54 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,28 @@
 # Your Code Here
+
+def map(array)
+  new = []
+  index = 0
+  while index < array.length
+    new << yield(array[index])
+    index += 1
+  end
+  new
+end
+
+
+def reduce(array, starting_point=nil)
+  if starting_point
+    sum = starting_point
+    index = 0
+  else
+    sum = array[0]
+    index = 1
+  end
+  while index < array.length do
+    sum = yield(sum, array[index])
+    index += 1
+  end
+  sum
+end
+  
